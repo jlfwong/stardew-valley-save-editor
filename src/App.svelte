0d7d070b5e1ef2422f43235c0ab982bc6caa1a81
Jamie Wong <jamie.lf.wong@gmail.com>
2020-03-28 16:08:01 -0700

    Basic money hacking

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,66 @@
 <script>
-	import {handleFileDrop} from './parser.ts';
+  import {handleFileDrop} from './parser.ts';
 
-	let isDragging = false
+  let isDragging = false
+  let saveGame = null
 
-	function onDrop(ev) {
-		isDragging = false
-		handleFileDrop(ev)
-	}
+  function onDrop(ev) {
+    isDragging = false
+    handleFileDrop(ev, (droppedSaveGame) => {
+      saveGame = droppedSaveGame
+      window.saveGame = saveGame
+    })
+  }
 
-	function onDragOver(ev) {
-		isDragging = true
-		ev.preventDefault()
-	}
+  function onDragOver(ev) {
+    isDragging = true
+    ev.preventDefault()
+  }
 
-	function onDragEnd(ev) {
-		isDragging = false
-	}
+  function onDragEnd(ev) {
+    isDragging = false
+  }
+
+  function onButtonClick() {
+    saveGame.getHost().setMoney(99999)
+
+    saveGame = saveGame
+  }
+
+  function download() {
+    saveGame.download()
+  }
 </script>
 
 <main on:drop={onDrop} on:dragover={onDragOver} on:dragend={onDragEnd} class={isDragging ? "isDragging" : ""}>
-Drop file here
+{#if saveGame != null}
+  <button on:click={onButtonClick}>MONEY!</button>
+  <button on:click={download}>Download</button>
+  {saveGame.getHost().getMoney()}
+{:else}
+  Drop file here
+{/if}
 </main>
 
 <style>
-	:global(html), :global(body) {
-		margin: 0;
-		padding: 0;
-		overflow: hidden;
-	}
+  :global(html), :global(body) {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
 
-	:global(*) {
-		box-sizing: border-box;
-	}
+  :global(*) {
+    box-sizing: border-box;
+  }
 
-	main {
-		text-align: center;
-		border: 1px solid red;
-		height: 100vh;
-		width: 100vw;
-	}
+  main {
+    text-align: center;
+    border: 1px solid red;
+    height: 100vh;
+    width: 100vw;
+  }
 
-	.isDragging {
-		outline: 3px solid blue;
-	}
+  .isDragging {
+    border: 3px solid blue;
+  }
 </style>
--- src/parser.ts
+++ src/parser.ts
@@ -1,25 +1,87 @@
-async function processDroppedFile(file: File) {
-  const xmlContent = await file.text()
-  const parser = new DOMParser()
-  const doc = parser.parseFromString(xmlContent, "application/xml")
-  console.log(doc)
+class Player {
+  constructor(private element: Element) {}
+
+  getMoney(): number {
+    return parseInt(this.element.querySelector("money").textContent.trim())
+  }
+
+  setMoney(value: number) {
+    this.element.querySelector("money").textContent = value.toFixed(0)
+  }
 }
 
-export function handleFileDrop(ev: DragEvent) {
+class SaveGame {
+  private doc: XMLDocument
+  private host: Element
+  private farm: Element
+  private farmHouse: Element
+  private buildings: Element[]
+  private cabins: Element[]
+  private farmhands: Element[]
+
+  constructor(private filename: string, xmlContent: string) {
+    const parser = new DOMParser()
+    this.doc = parser.parseFromString(xmlContent, "application/xml")
+    this.host = this.doc.querySelector(":scope > player")
+
+    const locations = Array.from(this.doc.querySelector(":scope > locations").children)
+
+    this.farm = locations.find(x => x.getAttribute("xsi:type") == "Farm")
+    this.farmHouse = locations.find(x => x.getAttribute("xsi:type") == "FarmHouse")
+    this.buildings = Array.from(this.farm.querySelector(":scope > buildings").children)
+    this.cabins = this.buildings.filter(b => {
+      const indoors = b.querySelector(":scope > indoors")
+      if (!indoors) return false
+      return indoors.getAttribute("xsi:type") == "Cabin"
+    })
+    this.farmhands = this.cabins.map(cabin => cabin.querySelector(":scope > indoors").querySelector("farmhand"))
+  }
+
+  getHost(): Player {
+    return new Player(this.host)
+  }
+
+  download(): void {
+    console.log('Downloading as', this.filename)
+
+    const serializer = new XMLSerializer
+
+    // We use an incorrect mime-type of application/octet-stream to prevent Chrome from appending
+    // a file extension, since Stardew Valley's save files don't have file extensions.
+    const blob = new Blob([serializer.serializeToString(this.doc)], {type: 'application/octet-stream'})
+
+    const a = document.createElement('a')
+    a.download = this.filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+
+    // For this to work in Firefox, the <a> must be in the DOM
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+  }
+}
+
+async function processDroppedFile(file: File): Promise<SaveGame> {
+  const xmlContent = await file.text()
+  return new SaveGame(file.name, xmlContent)
+}
+
+export async function handleFileDrop(ev: DragEvent, cb: (sg: SaveGame) => void) {
   ev.preventDefault()
 
   if (ev.dataTransfer.items) {
     for (var i = 0; i < ev.dataTransfer.items.length; i++) {
       if (ev.dataTransfer.items[i].kind === 'file') {
         var file = ev.dataTransfer.items[i].getAsFile();
-        processDroppedFile(file)
+        cb(await processDroppedFile(file))
         return
       }
     }
   } else {
     // Use DataTransfer interface to access the file(s)
     for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-      processDroppedFile(ev.dataTransfer[i])
+      cb(await processDroppedFile(ev.dataTransfer[i]))
       return
     }
   }
